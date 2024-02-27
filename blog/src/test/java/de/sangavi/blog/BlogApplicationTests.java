e31b84d31d762a7e37fb57c15ccaee669f232c96
Sangavi Shanthakumar <92029067+sangavishanthakumar@users.noreply.github.com>
2024-02-27 22:11:02 +0100

    tests + pom.xml

---
+++ blog/src/test/java/de/sangavi/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sangavi.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
